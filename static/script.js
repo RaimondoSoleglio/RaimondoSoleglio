bdbef233a0e45c0e78d71ca599f9ec0b768d887b
Bob <168981911+RaimondoSoleglio@users.noreply.github.com>
2024-11-24 11:22:50 +0000

    Sun, Nov 24, 2024, 11:22 AM +00:00

--- static/script.js
+++ static/script.js
@@ -117,7 +117,8 @@ addPlayerButton.addEventListener('click', () => {
     if (playerNames.length < maxPlayers) {
         playerLabel.textContent = `Player ${playerNames.length + 1} Name:`;
     } else {
-       //  playerNamesSection.style.display = 'none';
+        playerLabel.style.display = 'none';
+        playerLabel.style.display = 'none';
         timerSection.style.display = 'inline';
         submitButton.style.display = 'inline';
     }
