7c34f49f4e0c4f9432a309d04929463a08845efb
Bob <168981911+RaimondoSoleglio@users.noreply.github.com>
2024-12-08 12:14:16 +0000

    Sun, Dec 8, 2024, 12:14 PM +00:00

--- static/scriptTimer.js
+++ static/scriptTimer.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const timerElement = document.getElementById("timer");
 
-    let timerCounter = parseInt(timerContainer.dataset.timer, 10);
+    let timerCounter = parseInt(timerElement.dataset.timer, 10);
     console.log(timerCounter);
     const interval = setInterval(() => {
         if (timerCounter <= 1) {
